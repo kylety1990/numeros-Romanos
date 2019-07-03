d854faf11d0218e8a580262dc9e8df686fa5ff76
christian Albano Gordano <cristian001_13@hotmail.com>
2019-07-03 15:52:27 +0200

    Case number 1 RED!!

---
+++ spec/numerosRomanosSpec.js
@@ -0,0 +1,14 @@
+describe("Numeros Romanos", function() {
+  
+
+  it(" When the number is one", function() {
+    const number = 1;
+
+    const result = tipeNumber(number)
+
+    expect(result).toEqual(1);
+
+    
+  });
+
+});
