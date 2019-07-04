7e950a13c06d72e0d5fdbd366746e38924c67520
christian Albano Gordano <cristian001_13@hotmail.com>
2019-07-04 15:39:01 +0200

    Resolved all conversions

--- spec/numerosRomanosSpec.js
+++ spec/numerosRomanosSpec.js
@@ -20,6 +20,15 @@ describe("Numeros Romanos", function() {
     
   });
 
+  it("define all the numbers", function() {
+    const number = "M";
+
+    const result = tipeNumber(number)
+
+    expect(result).toEqual(1000);
+
+    
+  });
 });
 
 
@@ -31,5 +40,20 @@ let result = number.toLowerCase()
   }
   else if( result === 'v'){
     return 5;
+  }
+  else if( result === 'x'){
+    return 10;
+  }
+  else if( result === 'l'){
+    return 50;
+  }
+  else if( result === 'c'){
+    return 100;
+  }
+  else if (result === 'd'){
+    return 500;
+  }
+  else if(result === 'm'){
+    return 1000;
   }
 }
