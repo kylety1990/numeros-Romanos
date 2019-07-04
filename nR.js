904e80de943314f9a44fe5612d79326f31b76001
christian Albano Gordano <cristian001_13@hotmail.com>
2019-07-04 17:17:58 +0200

    convert the romanic number to arabic number RED!!ç

--- nR.js
+++ nR.js
@@ -1,30 +1,46 @@
 function tipeNumber(number){
+    
+    let result = []
 
-    let result = number.toLowerCase()
-      if(result === 'i'){
-        return 1;
+    for(let i = 0; i<number.length;i++){
+
+      if(number[i] === 'i'){
+        
+        result.push(1)
+        
       }
-      else if( result === 'v'){
-        return 5;
+      else if(number[i] === 'v'){
+        
+        result.push(5)
+        
       }
-      else if( result === 'x'){
-        return 10;
+      else if(number[i] === 'x'){
+        
+        result.push(10)
+        
       }
-      else if( result === 'l'){
-        return 50;
+      else if(number[i] === 'l'){
+        
+        result.push(50)
+        
       }
-      else if( result === 'c'){
-        return 100;
+      else if(number[i] === 'c'){
+        
+        result.push(100)
+        
       }
-      else if (result === 'd'){
-        return 500;
+      else if (number[i] === 'd'){
+        
+        result.push(500)
+        
       }
-      else if(result === 'm'){
-        return 1000;
-      }
-      else{
-        return 'incorrect number'
-      }
+      else if(number[i] === 'm'){
+        
+        result.push(1000)
+        
+      }  
+    } 
+    return result;
     }
 
     function equal(number){
@@ -68,11 +84,11 @@ function tipeNumber(number){
 
             if(correct === false){
                 
-                resultIncorrect.push(number[i])
+                resultIncorrect.push(number[i].toLowerCase())
                 
             }
             else{
-                resultCorrect.push(number[i]);
+                resultCorrect.push(number[i].toLowerCase());
 
             }
         }
@@ -88,7 +104,11 @@ function tipeNumber(number){
         
     }
 
-    let number = "Xoppi"
+    let number = "Xii"
     //console.log(equal(number))
     //console.log(tipeNumber("l"))
-    console.log(isCorrect(number))
+    let toma = isCorrect(number);
+    console.log(toma)
+    let vamos= tipeNumber(toma)
+    console.log(vamos)
+    
