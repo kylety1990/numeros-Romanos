369b17551e36ad035b8a8f6e2f797e4f70821a43
Christian <cristian001_13@hotmail.com>
2019-07-09 11:04:44 +0200

    Resolved case sum numeric arabic

--- nR.js
+++ nR.js
@@ -1,149 +1,132 @@
-function tipeNumber(number){
-    
+function tipeNumber(number) {
+
     let result = []
 
-    for(let i = 0; i<number.length;i++){
+    for (let i = 0; i < number.length; i++) {
 
-      if(number[i] === 'i'){
-        
-        result.push(1)
-        
-      }
-      else if(number[i] === 'v'){
-        
-        result.push(5)
-        
-      }
-      else if(number[i] === 'x'){
-        
-        result.push(10)
-        
-      }
-      else if(number[i] === 'l'){
-        
-        result.push(50)
-        
-      }
-      else if(number[i] === 'c'){
-        
-        result.push(100)
-        
-      }
-      else if (number[i] === 'd'){
-        
-        result.push(500)
-        
-      }
-      else if(number[i] === 'm'){
-        
-        result.push(1000)
-        
-      }
-      else{
-        return false
-      }
-    } 
-    return result;
-    }
+        if (number[i] === 'i') {
 
-    function equal(number){
-        let result = number.toLowerCase()
+            result.push(1)
 
-        if(result === 'i'){
-            return number.toLowerCase();
-          }
-          else if(result === 'v'){
-            return number.toLowerCase();
-          }
-          else if(result === 'x'){
-            return number.toLowerCase();
-          }
-          else if(result === 'l'){
-            return number.toLowerCase();
-          }
-          else if(result === 'c'){
-            return number.toLowerCase();
-          }
-          else if (result === 'd'){
-            return number.toLowerCase();
-          }
-          else if(result === 'm'){
-            return number.toLowerCase();
-          }
-          else{
+        } else if (number[i] === 'v') {
+
+            result.push(5)
+
+        } else if (number[i] === 'x') {
+
+            result.push(10)
+
+        } else if (number[i] === 'l') {
+
+            result.push(50)
+
+        } else if (number[i] === 'c') {
+
+            result.push(100)
+
+        } else if (number[i] === 'd') {
+
+            result.push(500)
+
+        } else if (number[i] === 'm') {
+
+            result.push(1000)
+
+        } else {
             return false
-          }
+        }
+    }
+    return result;
+}
+
+function equal(number) {
+    let result = number.toLowerCase()
+
+    if (result === 'i') {
+        return number.toLowerCase();
+    } else if (result === 'v') {
+        return number.toLowerCase();
+    } else if (result === 'x') {
+        return number.toLowerCase();
+    } else if (result === 'l') {
+        return number.toLowerCase();
+    } else if (result === 'c') {
+        return number.toLowerCase();
+    } else if (result === 'd') {
+        return number.toLowerCase();
+    } else if (result === 'm') {
+        return number.toLowerCase();
+    } else {
+        return false
+    }
+}
+
+function isCorrect(number) {
+
+    number.split('');
+
+    let resultCorrect = [];
+    let resultIncorrect = [];
+    for (let i = 0; i < number.length; i++) {
+
+        const correct = equal(number[i])
+
+        if (correct === false) {
+
+            resultIncorrect.push(number[i].toLowerCase())
+
+        } else {
+            resultCorrect.push(number[i].toLowerCase());
+
+        }
     }
 
-    function isCorrect(number){
-
-        number.split('');
-
-        let resultCorrect = [];
-        let resultIncorrect = [];
-        for ( let i= 0; i < number.length ; i++){
-            
-            const correct = equal(number[i])
-
-            if(correct === false){
-                
-                resultIncorrect.push(number[i].toLowerCase())
-                
-            }
-            else{
-                resultCorrect.push(number[i].toLowerCase());
-
-            }
-        }
-
-        if(resultIncorrect.length > 0 ){
-            return 'Datos incorrectos: ' + resultIncorrect;
-        }
-        else {
-            return resultCorrect;
-        }
-        
-        
+    if (resultIncorrect.length > 0) {
+        return 'Datos incorrectos: ' + resultIncorrect;
+    } else {
+        return resultCorrect;
     }
 
-   
-    function sumNumberArabic(number){
-      if(number === false){
-        return false;
-      }
-      else{
-      const suma = number.reduce(function(a, b){
-        return a + b
 
-      })
-        return suma;
-      }
-    }
+}
 
-    function changeValue(number){
 
-      let result= [];
-      for(let i=0; i<number.length;i++){
-        if(number[i] < number[i+1]){
-         result.push(number[i]-(number[i]*2));
-         
-        }else{
-          result.push(number[i]) 
+
+
+function changeValue(number) {
+
+    let result = [];
+    for (let i = 0; i < number.length; i++) {
+        if (number[i] < number[i + 1]) {
+            result.push(number[i] - (number[i] * 2));
+
+        } else {
+            result.push(number[i])
         }
 
-      }
-      return result
     }
-    let number = "xic"
-    
-   
-    let toma = isCorrect(number);
-    console.log(toma)
-    let vamos= tipeNumber(toma)
-    console.log(vamos)
-    let tomate= changeValue(vamos)
-    console.log(tomate)
-    let casi = sumNumberArabic(tomate)
+    return result
+}
 
-    console.log(casi)
+function sumNumberArabic(number) {
 
+    for (let i = 0; i < number.length; i++) {
+        if (number[i] > number[i + 1]) {
+            number[i + 1] += number[i];
+            number.shift()
+            return number
+        } else {
+
+        }
+    }
+}
+
+let number = "xi"
+
+
+let toma = isCorrect(number);
+console.log(toma)
+let vamos = tipeNumber(toma)
+console.log(vamos)
+let casi = sumNumberArabic(vamos)
+console.log(casi)
--- spec/numerosRomanosSpec.js
+++ spec/numerosRomanosSpec.js
@@ -1,173 +1,148 @@
 describe("Numeros Romanos", function() {
-  
-  
-  it("Some number of the chain is incorrect", function() {
-    const number = "xii";
 
-    const result = isCorrect(number)
 
-    expect(result).toEqual(['x','i','i']);
-    
-  });
+    it("Some number of the chain is incorrect", function() {
+        const number = "xii";
 
-  it("convert the numbers to arabicas", function() {
-    
-    const number = ['x','i'];
+        const result = isCorrect(number)
 
-    const result = tipeNumber(number)
+        expect(result).toEqual(['x', 'i', 'i']);
 
-    expect(result).toEqual([10,1]);
-    
-  });
- 
-  it("two numbers are added", function() {
-    
-    const number = [10,1];
+    });
 
-    const result = sumNumberArabic(number)
+    it("convert the numbers to arabicas", function() {
 
-    expect(result).toEqual(11);
+        const number = ['x', 'i'];
 
-  });
-  
-  it("numbers that change their value", function() {
-    
-    const number = [1,10];
+        const result = tipeNumber(number)
 
-    const result = changeValue(number)
+        expect(result).toEqual([10, 1]);
 
-    expect(result).toEqual([-1,10]);
+    });
 
-  });
+    it("two numbers are added", function() {
+
+        const number = [10, 1];
+
+        const result = sumNumberArabic(number)
+
+        expect(result).toEqual([11]);
+
+    });
+
+
+    // it("when subtracting Roman numerals", function() {
+
+    //     const number = [1, 10];
+
+    //     const result = subtractNumbers(number)
+
+    //     expect(result).toEqual([9]);
+
+    // });
 });
 
 
-function equal(number){
-  let result = number.toLowerCase()
+function equal(number) {
+    let result = number.toLowerCase()
 
-  if(result === 'i'){
-      return number.toLowerCase();
-    }
-    else if(result === 'v'){
-      return number.toLowerCase();
-    }
-    else if(result === 'x'){
-      return number.toLowerCase();
-    }
-    else if(result === 'l'){
-      return number.toLowerCase();
-    }
-    else if(result === 'c'){
-      return number.toLowerCase();
-    }
-    else if (result === 'd'){
-      return number.toLowerCase();
-    }
-    else if(result === 'm'){
-      return number.toLowerCase();
-    }
-    else{
-      return false
+    if (result === 'i') {
+        return number.toLowerCase();
+    } else if (result === 'v') {
+        return number.toLowerCase();
+    } else if (result === 'x') {
+        return number.toLowerCase();
+    } else if (result === 'l') {
+        return number.toLowerCase();
+    } else if (result === 'c') {
+        return number.toLowerCase();
+    } else if (result === 'd') {
+        return number.toLowerCase();
+    } else if (result === 'm') {
+        return number.toLowerCase();
+    } else {
+        return false
     }
 }
 
-function isCorrect(number){
+function isCorrect(number) {
 
-  number.split('');
+    number.split('');
 
-  let resultCorrect = [];
-  let resultIncorrect = [];
-  for ( let i= 0; i < number.length ; i++){
-      
-      const correct = equal(number[i])
+    let resultCorrect = [];
+    let resultIncorrect = [];
+    for (let i = 0; i < number.length; i++) {
 
-      if(correct === false){
-          
-          resultIncorrect.push(number[i])
-          
-      }
-      else{
-          resultCorrect.push(number[i]);
+        const correct = equal(number[i])
 
-      }
-  }
+        if (correct === false) {
 
-  if(resultIncorrect.length > 0 ){
-      return 'Datos incorrectos: ' + resultIncorrect;
-  }
-  else {
-      return resultCorrect;
-  }
-  
-}
+            resultIncorrect.push(number[i])
 
-function tipeNumber(number){
-    
-  let result = []
+        } else {
+            resultCorrect.push(number[i]);
 
-  for(let i = 0; i < number.length ; i++){
-
-    if(number[i] === 'i'){
-      
-      result.push(1)
-      
-    }
-    else if(number[i] === 'v'){
-      
-      result.push(5)
-      
-    }
-    else if(number[i] === 'x'){
-      
-      result.push(10)
-      
-    }
-    else if(number[i] === 'l'){
-      
-      result.push(50)
-      
-    }
-    else if(number[i] === 'c'){
-      
-      result.push(100)
-      
-    }
-    else if (number[i] === 'd'){
-      
-      result.push(500)
-      
-    }
-    else if(number[i] === 'm'){
-      
-      result.push(1000)
-      
-    }  
-  } 
-  return result;
-  }
-
-    
-    
-  function sumNumberArabic(number){
-        
-    const suma = number.reduce(function(a, b){
-      return a + b
-
-    })
-      return suma;
-    }
-  
-    function changeValue(number){
-
-      let result= [];
-      for(let i=0; i<number.length;i++){
-        if(number[i] < number[i+1]){
-         result.push(number[i]-(number[i]*2));
-         
-        }else{
-          result.push(number[i]) 
         }
-
-      }
-      return result
     }
+
+    if (resultIncorrect.length > 0) {
+        return 'Datos incorrectos: ' + resultIncorrect;
+    } else {
+        return resultCorrect;
+    }
+
+}
+
+function tipeNumber(number) {
+
+    let result = []
+
+    for (let i = 0; i < number.length; i++) {
+
+        if (number[i] === 'i') {
+
+            result.push(1)
+
+        } else if (number[i] === 'v') {
+
+            result.push(5)
+
+        } else if (number[i] === 'x') {
+
+            result.push(10)
+
+        } else if (number[i] === 'l') {
+
+            result.push(50)
+
+        } else if (number[i] === 'c') {
+
+            result.push(100)
+
+        } else if (number[i] === 'd') {
+
+            result.push(500)
+
+        } else if (number[i] === 'm') {
+
+            result.push(1000)
+
+        }
+    }
+    return result;
+}
+
+
+
+function sumNumberArabic(number) {
+
+    for (let i = 0; i < number.length; i++) {
+        if (number[i] > number[i + 1]) {
+            number[i + 1] += number[i];
+            number.shift()
+            return number
+        } else {
+
+        }
+    }
+}
